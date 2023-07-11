4c637b4483efebfa2b10c5dd9c82fb9a48b26167
saisrinisrinivas <102722606+saisrinisrinivas@users.noreply.github.com>
2023-07-11 12:35:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git clone -b main 'https://github.com/saisrinisrinivas/nodejsrepo.git'
-docker build -t public.ecr.aws/s9e0w9o5/demojenkins:latest
+docker build -t public.ecr.aws/s9e0w9o5/demojenkins:latest .
