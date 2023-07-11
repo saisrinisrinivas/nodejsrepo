d83cb70183420b60f9171a6a3a2ab77019d9a60b
saisrinisrinivas <102722606+saisrinisrinivas@users.noreply.github.com>
2023-07-11 12:33:54 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sh 'aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/s9e0w9o5'
-sh 'docker tag demojenkins:latest public.ecr.aws/s9e0w9o5/demojenkins:latest'
-sh 'docker push public.ecr.aws/s9e0w9o5/demojenkins:latest'
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/s9e0w9o5
+docker tag demojenkins:latest public.ecr.aws/s9e0w9o5/demojenkins:latest
+docker push public.ecr.aws/s9e0w9o5/demojenkins:latest
