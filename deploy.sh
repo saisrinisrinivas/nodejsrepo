076b4d58b61f75b05e4cf64dc1128ad8b02d8ae4
saisrinisrinivas <102722606+saisrinisrinivas@users.noreply.github.com>
2023-07-11 12:32:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
+docker pull public.ecr.aws/s9e0w9o5/demojenkins:latest 
+docker run -itd --network=host public.ecr.aws/s9e0w9o5/demojenkins:latest       
   
-  sh '''
-      docker pull public.ecr.aws/s9e0w9o5/demojenkins:latest 
-      docker run -itd --network=host public.ecr.aws/s9e0w9o5/demojenkins:latest       
-      '''
